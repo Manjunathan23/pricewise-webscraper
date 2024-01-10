1a86699767c134a811f0594b4b9e668b3b0e5b90
Manjunathan23 <104411355+Manjunathan23@users.noreply.github.com>
2024-01-10 22:05:57 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['m.media-amazon.com', 'www.amazon.in']
+  }
+}
 
 module.exports = nextConfig
